4f6c9741cf4703d2ec009bdba0efbd87d42894db
yda288 <yda288@users.noreply.github.com>
2017-01-02 17:08:59 +0000

    Initial commit of boostmappings and addition of boost_map to Instance and Tuple types

---
+++ mypy/boostmappings.py
@@ -0,0 +1,12 @@
+
+
+mappings = { 'builtins.float' : 'double',
+             'builtins.str' : 'string',
+             'builtins.tuple' : 'tuple',
+             'builtins.int' : 'int',
+             'builtins.complex' : 'complex',
+             'builtins.unicode' : 'string',
+             'builtins.list' : 'vector',
+             'builtins.dict' : 'map',
+             'numpy.ndarray' : 'ndarray'
+             }
